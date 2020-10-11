75ad8ec9159ecb7f7dee66e1b2f609c7455a042d
Jessica Lynch <jessica.lynch@trojans.dsu.edu>
2020-10-11 15:10:17 -0400

    Update driver and add comments

--- fib.py
+++ fib.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
-errors = ["Error: sequence index must be 1 or greater"]
+"""fib.py: fibonacci sequence functions"""
+__author__ = "Jessica Lynch"
+
+# Global error messages
+errors = ["Sequence index must be 1 or greater",
+          "Exponent must be positive",
+          "Integer must be positive"
+          "Value must be integer"
+          ]
 
 
 def fib_recur(x):
@@ -14,6 +22,8 @@ def fib_recur(x):
 
 
 def fib_cache(x):
+    """Returns the xth value of the fibonacci sequence via recursion
+    and a cache table to avoid repeat calculations"""
     if x < 1:
         raise ValueError(errors[0])
     else:
@@ -22,9 +32,7 @@ def fib_cache(x):
 
 
 def fib_cache_helper(x, cache):
-    """Returns the xth value of the fibonacci sequence via recursion
-    and a cache table to avoid duplicate calculations"""
-    # print(x, cache)
+    """Helper function for fib_cache"""
     if x == 0 or x == 1:
         return x
     elif x in cache:
@@ -35,6 +43,7 @@ def fib_cache_helper(x, cache):
 
 
 def fib_loop(x):
+    """Returns xth value in fibonacci sequence via iteration"""
     if x < 1:
         raise ValueError(errors[0])
     if x == 1:
@@ -50,6 +59,7 @@ def fib_loop(x):
 
 
 def fib_matrix(x):
+    """Returns xth value in fibonacci sequence via matrix multiplication"""
     if x < 1:
         raise ValueError(errors[0])
     if x == 1:
@@ -61,6 +71,9 @@ def fib_matrix(x):
 
 
 def int_power(x, y):
+    """Returns x^y via binary exponentiation"""
+    if y < 0:
+        raise ValueError(errors[1])
     binary_digits = base10_to_binary(y)
     binary_digits.reverse()
     product, i = 1, 1
@@ -72,6 +85,9 @@ def int_power(x, y):
 
 
 def int_power_brute(x, y):
+    """Returns x^y via brute force"""
+    if y < 0:
+        raise ValueError(errors[1])
     product = 1
     for _ in range(y):
         product = product * x
@@ -79,6 +95,11 @@ def int_power_brute(x, y):
 
 
 def base10_to_binary(x):
+    """Converts a base 10 integer to binary"""
+    if x < 0:
+        raise ValueError(errors[2])
+    elif not isinstance(x, int):
+        raise ValueError(errors[3])
     digits = []
     while x > 0:
         digits.insert(0, x % 2)
@@ -87,25 +108,39 @@ def base10_to_binary(x):
 
 
 def matrix_multiply(m1, m2):
+    """Multiplies two matrices together"""
     if len(m1[0]) != len(m2):
         return False
     return [[sum(a * b for a, b in zip(m1_row, m2_col)) for m2_col in zip(*m2)] for m1_row in m1]
 
 
 def matrix_power(m, y):
+    """Returns matrix raised to a power"""
     binary_digits = base10_to_binary(y)
     binary_digits.reverse()
     n = len(m)
+
+    # Create the matrix equivalent to the integer 1
     identity_matrix = [[int(i == j) for i in range(n)] for j in range(n)]
+
+    # Init final matrix with identity matrix
     product = identity_matrix
+
+    # Loop through the binary digits
     for digit in binary_digits:
+
+        # If the digit is a 1, multiply by the current matrix power
         if digit:
             product = matrix_multiply(product, m)
+
+        # Square the matrix every time to increase the power
         m = matrix_multiply(m, m)
+
     return product
 
 
 def matrix_print(m):
+    """Prints a matrix"""
     for i in range(len(m)):
         for j in range(len(m[i])):
             print(f"{m[i][j]} ", end="")
--- main.py
+++ main.py
@@ -1,20 +1,173 @@
 #!/usr/bin/env python3
 
+"""main.py: CSC 482 Lab0x04 driver program"""
+__author__ = "Jessica Lynch"
+
+from os.path import dirname, join
+from time import perf_counter_ns, time_ns
+from math import sqrt, log
+import sys
 import fib
+
+# Route print statements to file
+current_dir = dirname(__file__)
+file_name = "output_" + str(time_ns()) + ".txt"
+file_path = join(current_dir, file_name)
+sys.stdout = open(file_path, "w")
+
+
+def get_expected_dr(func_name, x):
+    """Returns the expected doubling ratio for each fibonacci function"""
+    if func_name == "fib_recur":  # Exponential time
+        phi = (1 + sqrt(5)) / 2
+        return (phi ** x) / (phi ** (x // 2))
+    elif func_name in ("fib_cache", "fib_loop"):  # Linear time
+        return x / (x // 2)  # or 2
+    elif func_name == "fib_matrix":  # Logarithmic time
+        x_size = len(fib.base10_to_binary(x))
+        if log(x_size // 2, 2) > 0:
+            return (log(x_size, 2)) / (log(x_size // 2, 2))
+        else:
+            return False
 
 
 def main():
-    x = 25
-    try:
-        for i in range(1, x + 1):
-            print(fib.fib_cache(i), end=" ")
+    # Assign timer function to variable
+    clock = perf_counter_ns
+
+    # Determine max run time for each algorithm
+    one_second = 1000000000  # 1 second in nanoseconds
+    # MAX_RUN_TIME = one_second * 60 * 10
+    MAX_RUN_TIME = one_second  # small value for testing
+    # MAX_NUMBER = sys.maxsize
+    MAX_NUMBER = 25000
+
+    # Init string constants for table
+    t_str = "Time"
+    dr_str = "DR"
+    expected_str = "expected"
+    na_str = "--"
+
+    # Build list with functions to test
+    fib_funcs = [fib.fib_recur, fib.fib_cache, fib.fib_loop, fib.fib_matrix]
+    num_funcs = len(fib_funcs)
+    timed_out_funcs = [False] * num_funcs
+
+    # Print table header
+    col_width_full = 30
+    col_width_med = col_width_full // 3
+    col_width_small = 6
+
+    # Function names (header top row)
+    print(f"{'':>{col_width_small * 2}}", end="")
+    for i in range(num_funcs):
+        print(f"{fib_funcs[i].__name__:>{col_width_full}}", end="")
+    print()
+
+    # Data columns (header second row)
+    print(f"{'x':>{col_width_small}}", end="")
+    print(f"{'size':>{col_width_small}}", end="")
+    for i in range(num_funcs):
+        print(f"{t_str:>{col_width_med}}", end="")
+        print(f"{dr_str:>{col_width_med}}", end="")
+        print(f"{expected_str:>{col_width_med}}", end="")
+    print("\n")
+
+    cols_per_func = 3
+
+    # Init flag to track when all functions are complete
+    timed_out = False
+
+    # Init list of dictionaries to store timing results
+    results = []
+    for _ in range(num_funcs):
+        results.append({})
+
+    # Init list to store doubling ratios
+    doubling_ratio = [-1] * num_funcs
+
+    # Start testing algorithms with increasing x values
+    for x in range(1, MAX_NUMBER + 1):
+
+        # Exit if all functions are complete
+        if timed_out:
+            print("All functions timed out")
+            break
+
+        # Otherwise assume complete and keep testing
+        timed_out = True
+
+        # Print current x value and number of binary digits in x
+        x_size = len(fib.base10_to_binary(x))
+        print(f"{x:>{col_width_small}}", end="")
+        print(f"{x_size:>{col_width_small}}", end="")
+
+        # Loop through each function and test with current x value
+        for i in range(num_funcs):
+
+            # Skip to next function if current is timed out
+            if timed_out_funcs[i]:
+                # Print filler values to maintain table structure
+                for _ in range(cols_per_func):
+                    print(f"{na_str:>{col_width_med}}", end="")
+                if i == (num_funcs - 1):
+                    print()
+                continue
+
+            try:
+                # Start clock
+                t0 = clock()
+
+                # Run algorithm
+                fib_num = fib_funcs[i](x)
+
+                # Stop clock and calculate time
+                t1 = clock() - t0
+
+                # Convert nanoseconds to milliseconds or seconds if possible
+                # and print time taken for current function
+                if t1 > 1000000000:
+                    s = t1 // 1000000000
+                    print(f"{str(s) + 's':>{col_width_med}}", end="")
+                elif t1 > 1000000:
+                    ms = t1 // 1000000
+                    print(f"{str(ms) + 'ms':>{col_width_med}}", end="")
+                else:
+                    print(f"{str(t1) + 'ns':>{col_width_med}}", end="")
+
+                # Calculate doubling ratio
+                if x % 2 == 0 and x >= 4:
+                    expected_dr = get_expected_dr(fib_funcs[i].__name__, x)
+                    half_x = x // 2
+                    doubling_ratio[i] = t1 / results[i][half_x]
+                    print(f"{doubling_ratio[i]:>{col_width_med}.2f}", end="")
+                    print(f"{expected_dr:>{col_width_med}.2f}", end="")
+                else:
+                    print(f"{'':>{col_width_med * 2}}", end="")
+
+                # Store time result from current run
+                results[i][x] = t1
+
+                # Update flags
+                if t1 < MAX_RUN_TIME:
+                    # At least one function is still going
+                    timed_out = False
+                else:
+                    # Current function is timed out
+                    timed_out_funcs[i] = True
+                    results[i].clear()
+
+            except RecursionError as e:
+                # Current function has reached maximum recursion depth,
+                # set flag and clear timing results dictionary
+                timed_out_funcs[i] = True
+                results[i].clear()
+            except ValueError as e:
+                # Something went wrong
+                print(e)
+                exit()
+
         print()
-        print("fib recur:", fib.fib_recur(x))
-        print("fib cache:", fib.fib_cache(x))
-        print("fib loop:", fib.fib_loop(x))
-        print("fib matrix:", fib.fib_matrix(x))
-    except Exception as e:
-        print(e)
 
 
 if __name__ == '__main__':
